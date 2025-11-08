6e0bc1453648349a91bf9102e5c1cf6a80c65fe2
Fecsx001 <fecsoandrasbalazs@gmail.com>
2025-11-08 13:26:19 +0100

    Added cors to backend

--- src/backend/app.py
+++ src/backend/app.py
@@ -25,6 +25,14 @@ app.add_middleware(
 
 app.include_router(routes_router)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
+
 
 @app.get("/")
 def route():
